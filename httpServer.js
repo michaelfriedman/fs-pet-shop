3ee409445f0198181cc059ba9ce8c56a87463b8a
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-14 01:07:20 -0800

    HTTP server handling requests for specific data.

--- httpServer.js
+++ httpServer.js
@@ -34,19 +34,37 @@ const server = http.createServer((req, res) => {
         return;
       }
       const pets = JSON.parse(petsJSON);
-      const petsJSON = JSON.stringify(pets[0]);
+      const petJSON = JSON.stringify(pets[0]);
 
+      res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
+    });
+  } else if (req.method === 'GET' && req.url === '/pets/1') {
+    fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Service Error');
+
+        return;
+      }
+
+      const pets = JSON.parse(petsJSON);
+      const petJSON = JSON.stringify(pets[1]);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
     });
   }
-
-
 });
 
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
 
 module.exports = server;
